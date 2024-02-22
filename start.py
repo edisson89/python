d34fc4fe85b2413349c9ef006814b802d357b2c9
edisson89 <edisson.12@hotmail.com>
2024-02-22 10:21:41 -0500

    Empezando a  usar python

---
+++ start.py
@@ -0,0 +1,11 @@
+"""
+Este módulo proporciona una función para multiplicar dos números.
+"""
+
+def multiply(x,y):
+    """"
+    Multiplica dos numeros
+    """
+    return x * y
+
+print( f"Esta multiplicación da como resultado : {multiply(55,9)}")
